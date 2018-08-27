5b1f959e229d6e322a93d805805816515f1e345d
devGW <doscmDev@gmail.com>
2018-08-27 21:35:38 +0900

    호스팅 서버 문제로 인한 변경
    change : 호스팅서버에서 contents 함수를 막아서 소스 변경

--- crawling.php
+++ crawling.php
@@ -1,12 +1,23 @@
 <?php
-header('Content-Type: application/json;charset=UTF-8');
-include "simple_html_dom.php";
-if(!$_GET[page]) $page = 1;
-else $page = $_GET[page];
-$html = file_get_html("pasing address");
-$tt = date("Ymd");
+    header('Content-Type: application/json;charset=UTF-8');
+    include "simple_html_dom.php";
+    if(!$_GET[page]) $page = 1;
+    else $page = $_GET[page];
+	$url = "http://tusso.tu.ac.kr/jsp/manage/restaurant/restaurant_menu.jsp";
+    $ch = curl_init();
 
-function json_encode2($data) {
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    $str = curl_exec($ch);
+
+	curl_close($ch);
+
+	$html = str_get_html($str);
+
+    $tt = date("Ymd");
+
+	function json_encode2($data) {
 	switch (gettype($data)) {
 		case 'boolean':
 		return $data?'true':'false';
@@ -16,33 +27,30 @@ function json_encode2($data) {
 		case 'string':
 		return '"'.strtr($data, array('\\'=>'\\\\','"'=>'\\"')).'"';
 		case 'array':
-		$rel = false; // rel,9ative array?
+		$rel = false; // relative array?
 		$key = array_keys($data);
 		foreach ($key as $v) {
-			if (!is_int($v)) {
-				$rel = true;
-				break;
-			}
+		if (!is_int($v)) {
+		$rel = true;
+		break;
+		}
 		}
 		$arr = array();
 		foreach ($data as $k=>$v) {
-			$arr[] = ($rel?'"'.strtr($k, array('\\'=>'\\\\','"'=>'\\"')).'":':'').json_encode2($v);
+		$arr[] = ($rel?'"'.strtr($k, array('\\'=>'\\\\','"'=>'\\"')).'":':'').json_encode2($v);
 		}
 		return $rel?'{'.join(',', $arr).'}':'['.join(',', $arr).']';
 		default:
 		return '""';
+		}
 	}
-}
-if($html)
-{
-	$json["success"] = "true";
-	$json["code"] = "200";
-
-	$i = 0;
-	foreach($html->find('tr') as $article) {
-		$json[$article->find('th', 0)->plaintext]['data'] = array($article->find('td', 0)->plaintext);
-		$i++;
-	}
-	file_put_contents("./menu.json", json_encode2($json));
-}
+    if($html)
+    {
+		$i = 0;
+		foreach($html->find('tr') as $article) {
+			$json[$article->find('th', 0)->plaintext]['data'] = array($article->find('td', 0)->plaintext);
+			$i++;
+		}
+		file_put_contents("./menu.json", json_encode2($json));
+    }
 ?>
