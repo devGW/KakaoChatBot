f8981b6c9e44f6bdbed70838c1a1caabf0e75eaf
devGW <doscmDev@gmail.com>
2018-08-30 18:56:44 +0900

    중복 코드 제거
    remove : 중복코드
    tryCatch : json 파일이 생성되지 않았을때 에러가 발생하는데 아직 잡지 못함

--- message.php
+++ message.php
@@ -4,15 +4,7 @@
 
 
     $tt = date(Ymd);
-    $json = file_get_contents("./".$tt."menu.json");
-    $result_json = json_decode($json, true);
 
-    $key = array();
-    $key = array_keys($result_json);
-
-    $data = json_decode(file_get_contents('php://input'), true);
-    $content = $data["content"];
-    $tt = date(Ymd);
     $json = file_get_contents("./".$tt."menu.json");
     $result_json = json_decode($json, true);
     $key = array();
@@ -27,7 +19,7 @@
           $mainMenu[$i] = trim(mb_substr($result_json[$key[$i]][data][0],0,18,'UTF-8').', '.mb_substr($result_json[$key[$i]][data][0],38,8,'UTF-8')).', '.
           mb_substr($result_json[$key[$i]][data][0],66,7,'UTF-8').', '.mb_substr($result_json[$key[$i]][data][0],94,12,'UTF-8').', '.
           mb_substr($result_json[$key[$i]][data][0],127,7,'UTF-8').', '.mb_substr($result_json[$key[$i]][data][0],155,7,'UTF-8');
-
+          //문제 발생시 strpos 이용
       } else {
         $mainMenu[$i] = trim($result_json[$key[$i]][data][0]);
       }
@@ -35,6 +27,7 @@
     }
     $studentMenu = "";
     $professorMenu ="";
+    $info = "창업동아리 : NULL \n\n 가입문의 : 010480853825 \n\n 제작 : 정건우(컴공)";
     for ($i=0; $i < count($key); $i++) {
       if(strpos($title[$i], "특정식") !==false){
         $professorMenu = $professorMenu.$title[$i]." : ".$mainMenu[$i]."\\n";
@@ -42,6 +35,9 @@
         $studentMenu = $studentMenu.$title[$i]." : ".$mainMenu[$i]."\\n\\n";
       }
     }
+
+
+
 
 
     switch($content)
@@ -56,7 +52,7 @@
                     "keyboard":
                     {
                         "type": "buttons",
-                        "buttons": ["학생식당", "교직원식당", "menu3"]
+                        "buttons": ["학생식당", "교직원식당", "정보&문의"]
                     }
                 }';
             break;
@@ -70,21 +66,21 @@
                     "keyboard":
                     {
                         "type": "buttons",
-                        "buttons": ["학생식당", "교직원식당", "menu3"]
+                        "buttons": ["학생식당", "교직원식당", "정보&문의"]
                     }
                 }';
             break;
-        case "menu3":
+        case "정보&문의":
             echo '
                 {
                     "message":
                     {
-                        "text": "메뉴3을 선택하셨습니다."
+                        "text": "창업동아리 : NULL\n\n가입&피드백 : 01048085382\n\n제작 : 정건우(컴공)"
                     },
                     "keyboard":
                     {
                         "type": "buttons",
-                        "buttons": ["학생식당", "교직원식당", "menu3"]
+                        "buttons": ["학생식당", "교직원식당", "정보&문의"]
                     }
                 }';
             break;
@@ -98,7 +94,7 @@
                     "keyboard":
                     {
                         "type": "buttons",
-                        "buttons": ["학생식당", "교직원식당", "menu3"]
+                        "buttons": ["학생식당", "교직원식당", "정보&문의"]
                     }
                 }';
             break;
