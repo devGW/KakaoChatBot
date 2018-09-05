23d897ddfc4ac44a08d50517c8500d0e98efc0e3
gunwoo.jung <doscmDev@gmail.com>
2018-09-05 20:23:39 +0900

    decrease&add : 코드 단축 및 추가
    기존 코드를 단순하게 만들었으며 json 파일(기숙사식당 메뉴) 을 추가적으로 불러옴

--- message.php
+++ message.php
@@ -1,112 +1,150 @@
 <?php
-    $data = json_decode(file_get_contents('php://input'), true);
-    $content = $data["content"];
+$data = json_decode(file_get_contents('php://input'), true);
+$content = $data["content"];
+header('Content-Type: text/html; charset=utf-8');
+
+$tt = date(Ymd);
+$ch = true;
+$isUniv = file_exists("./univMenu/".$tt."menu.json");
+$isDormi = file_exists("./dormiMenu/".$tt."dormitoryMenu.json");
+
+if($isUniv){
+
+    $univJson = file_get_contents("./univMenu/".$tt."menu.json");
+    $dormiJson = file_get_contents("./dormiMenu/".$tt."dormitoryMenu.json");
+    $result_univJson = json_decode($univJson, true);
+    $result_dormiJson = json_decode($dormiJson, true);
 
 
-    $tt = date(Ymd);
-    $ch = true;
-    $isFile = file_exists("./".$tt."menu.json");
-    if($isFile){
 
-    $json = file_get_contents("./".$tt."menu.json");
-    $result_json = json_decode($json, true);
     $key = array();
-    $key = array_keys($result_json);
-
+    $key = array_keys($result_univJson);
     $title = array();
     $mainMenu = array();
+
+    $dormiKey = array();
+    $dormiKey = array_keys($result_dormiJson);
+    $dormiTitle = array();
+    $dormiMainMenu = array();
+
+    $studentMenu = "";
+    $professorMenu ="";
+    $dormiBreakMenu = "";
+    $dormiDinnerMenu ="";
+
     for($i=0; $i<count($key);$i++){
       $temp = strpos($key[$i],"M");
       $title[$i] = mb_substr($key[$i], 0,$temp/3);
-      if(strpos($result_json[$key[$i]][data][0], "돈까스") !== false){
-          $mainMenu[$i] = trim(mb_substr($result_json[$key[$i]][data][0],0,18,'UTF-8').', '.mb_substr($result_json[$key[$i]][data][0],38,8,'UTF-8')).', '.
-          mb_substr($result_json[$key[$i]][data][0],66,7,'UTF-8').', '.mb_substr($result_json[$key[$i]][data][0],94,12,'UTF-8').', '.
-          mb_substr($result_json[$key[$i]][data][0],127,7,'UTF-8').', '.mb_substr($result_json[$key[$i]][data][0],155,7,'UTF-8');
+      if(strpos($result_univJson[$key[$i]][data][0], "돈까스") !== false){
+          $mainMenu[$i] = trim(mb_substr($result_univJson[$key[$i]][data][0],0,18,'UTF-8').', '.mb_substr($result_univJson[$key[$i]][data][0],38,8,'UTF-8')).', '.
+          mb_substr($result_univJson[$key[$i]][data][0],66,7,'UTF-8').', '.mb_substr($result_univJson[$key[$i]][data][0],94,12,'UTF-8').', '.
+          mb_substr($result_univJson[$key[$i]][data][0],127,7,'UTF-8').', '.mb_substr($result_univJson[$key[$i]][data][0],155,7,'UTF-8');
           $ch = false;
           //문제 발생시 strpos 이용
       } else {
-        $mainMenu[$i] = trim($result_json[$key[$i]][data][0]);
+        $mainMenu[$i] = trim($result_univJson[$key[$i]][data][0]);
       }
-
-    }
-    $studentMenu = "";
-    $professorMenu ="";
-
-    for ($i=0; $i < count($key); $i++) {
       if(strpos($title[$i], "특정식") !==false){
-        $professorMenu = $professorMenu.$title[$i]." : ".$mainMenu[$i]."\\n";
+          $professorMenu = $professorMenu.$title[$i]." : ".$mainMenu[$i]."\\n";
       } else {
-        $studentMenu = $studentMenu.$title[$i]." : ".$mainMenu[$i]."\\n\\n";
+          $studentMenu = $studentMenu.$title[$i]." : ".$mainMenu[$i]."\\n\\n";
       }
     }
-  } else {
+    for ($i=0; $i <count($dormiKey); $i++) {
+      $dormiMainMenu[$i] = $result_dormiJson[$dormiKey[$i]][data][0];
+      $dormiTitle[$i] = $dormiKey[$i];
+
+      if(strpos($dormiTitle[$i], "조식") !==false){
+          $dormiBreakMenu = $dormiTitle[$i]." : ".$dormiMainMenu[$i]."\\n";
+      } else {
+          $dormiDinnerMenu = $dormiTitle[$i]." : ".$dormiMainMenu[$i]."\\n";
+      }
+
+    }
+
+
+
+} else {
     $studentMenu = "식당메뉴가 업로드 되지 않았습니다.";
     $professorMenu = "식당메뉴가 업로드 되지 않았습니다.";
-  }
-  if($ch){
+}
+if($ch){
     $studentMenu = "식당메뉴가 업로드 되지 않았습니다.";
     $professorMenu = "식당메뉴가 업로드 되지 않았습니다.";
     $ch = true;
-  }
-  $info = "창업동아리 : NULL \\n\\n가입문의 : 상담원 전환 클릭 \\n\\n제작 : https://github.com/devGW";
-    switch($content)
+}
+$info = "창업동아리 : NULL \\n\\n가입문의 : 상담원 전환 클릭 \\n\\n제작 : https://github.com/devGW";
+switch($content)
+{
+    case "학생식당":
+    echo '
     {
-            case "학생식당":
-                echo '
-                {
-                    "message":
-                    {
-                        "text": "'.$studentMenu.'"
-                    },
-                    "keyboard":
-                    {
-                        "type": "buttons",
-                        "buttons": ["학생식당", "교직원식당", "정보&문의"]
-                    }
-                }';
-            break;
-        case "교직원식당":
-            echo '
-                {
-                    "message":
-                    {
-                        "text": "'.$professorMenu.'"
-                    },
-                    "keyboard":
-                    {
-                        "type": "buttons",
-                        "buttons": ["학생식당", "교직원식당", "정보&문의"]
-                    }
-                }';
-            break;
-        case "정보&문의":
-            echo '
-                {
-                    "message":
-                    {
-                        "text": "'.$info.'"
-                    },
-                    "keyboard":
-                    {
-                        "type": "buttons",
-                        "buttons": ["학생식당", "교직원식당", "정보&문의"]
-                    }
-                }';
-            break;
-        default:
-            echo '
-                {
-                    "message":
-                    {
-                        "text": "잘못된 값입니다."
-                    },
-                    "keyboard":
-                    {
-                        "type": "buttons",
-                        "buttons": ["학생식당", "교직원식당", "정보&문의"]
-                    }
-                }';
-            break;
-    }
-
+        "message":
+        {
+            "text": "'.$studentMenu.'"
+            },
+            "keyboard":
+            {
+                "type": "buttons",
+                "buttons": ["학생식당", "교직원식당", "기숙사식당", "정보&문의"]
+            }
+        }';
+    break;
+  case "교직원식당":
+   echo '
+   {
+      "message":
+      {
+          "text": "'.$professorMenu.'"
+          },
+          "keyboard":
+          {
+              "type": "buttons",
+              "buttons": ["학생식당", "교직원식당", "기숙사식당", "정보&문의"]
+          }
+      }';
+      break;
+      case "정보&문의":
+      echo '
+      {
+          "message":
+          {
+              "text": "'.$info.'"
+              },
+              "keyboard":
+              {
+                  "type": "buttons",
+                  "buttons": ["학생식당", "교직원식당", "기숙사식당", "정보&문의"]
+              }
+          }';
+          break;
+          case "기숙사식당":
+           echo '
+           {
+              "message":
+              {
+                  "text": "'.$dormiBreakMenu."\\n".$dormiDinnerMenu.'"
+                  },
+                  "keyboard":
+                  {
+                      "type": "buttons",
+                      "buttons": ["학생식당", "교직원식당", "기숙사식당", "정보&문의"]
+                  }
+              }';
+              break;
+          default:
+          echo '
+          {
+              "message":
+              {
+                  "text": "잘못된 값입니다."
+                  },
+                  "keyboard":
+                  {
+                      "type": "buttons",
+                      "buttons": ["학생식당", "교직원식당", "기숙사식당", "정보&문의"]
+                  }
+              }';
+              break;
+          }
 ?>
