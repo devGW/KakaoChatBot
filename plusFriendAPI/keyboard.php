ab246f17354cc1d751d09e01cd80ccca260c1e21
gunwoo.jung <doscmDev@gmail.com>
2018-11-09 13:13:03 +0900

    add : keyboardAPI
    누락 됬던 파일 추가

---
+++ plusFriendAPI/keyboard.php
@@ -0,0 +1,9 @@
+﻿<?php
+ 
+   header('Content-Type: application/json; charset=utf-8');
+    echo '
+        {
+            "type": "buttons",
+            "buttons": ["학생식당", "교직원식당", "기숙사식당", "정보&문의"]
+        }';
+?>
